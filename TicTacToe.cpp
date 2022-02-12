993a5fc1332f1f8bd4fc125fe73a40d6894ece07
Andrew Higgins <anhi5992@colorado.edu>
2022-02-11 18:04:45 -0700

    added PlaceMarker

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
-using namespace std
+using namespace std;
 
-char CreateBoard(){
+char** CreateBoard(){
 	char TicTacBoard[3][3];
-	for(int i = 0; i <3; i++){
-		for(int j = 0; j < 3; j++){
-			TicTacBoard[i][j] = '-';
+	char** board = 0;
+	board = new char*[3];
+	for(int i = 0; i < 3; i++) {
+		board[i] = new char[3];
+		for(int j = 0; j < 3; j++) {
+			board[i][j] = '-';
 		}
 	}
+	return board;
+}
 
+void PlaceMarker(int i, int j, char** board char choice){
+	board[i][j] = choice;
 }
-void DisplayBoard(board* b){
-for(int i = 0;i<3;i++){
-	for(int j=0; j<3;j++){
-		cout << b[i][j] << " ";
-	}	
-	cout << endl;
+
+void DisplayBoard(char** board) {
+	for(int i = 0; i < 3; i++) {
+		for(int j = 0; j < 3; j++) {
+			cout << board[i][j];
+		}
+		cout << endl;
+	}
 }
-}
+
 
 int main(){
-	char board[][] = CreateBoard();
+	char** board = CreateBoard();
+	DisplayBoard(board);
 }
