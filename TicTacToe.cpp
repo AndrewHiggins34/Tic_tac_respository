9436bbb579def1cd28278a6c65ebcb31d82dfbce
Jack Rogers <jrogers2400@gmail.com>
2022-02-11 17:44:10 -0700

    Fixed CreateBoard and added DisplayBoard

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
-using namespace std
+using namespace std;
 
-int CreateBoard(){
+char** CreateBoard(){
 	char TicTacBoard[3][3];
-	for(int i = 0; i <3; i++){
-		for(int j = 0; j < 3; j++){
-			TicTacBoard[i][j] = '-';
+	char** board = 0;
+	board = new char*[3];
+	for(int i = 0; i < 3; i++) {
+		board[i] = new char[3];
+		for(int j = 0; j < 3; j++) {
+			board[i][j] = '-';
 		}
 	}
-
+	return board;
 }
+
+void DisplayBoard(char** board) {
+	for(int i = 0; i < 3; i++) {
+		for(int j = 0; j < 3; j++) {
+			cout << board[i][j];
+		}
+		cout << endl;
+	}
+}
+
+
 int main(){
-	char board[][] = CreateBoard();
+	char** board = CreateBoard();
+	DisplayBoard(board);
 }
