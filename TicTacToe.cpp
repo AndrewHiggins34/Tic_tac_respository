1822f9ef3d8a054ae06c58d87aa9cc6607af1773
Andrew Higgins <anhi5992@colorado.edu>
2022-02-11 17:54:43 -0700

    wrong info

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std
 
-int CreateBoard(){
+char CreateBoard(){
 	char TicTacBoard[3][3];
 	for(int i = 0; i <3; i++){
 		for(int j = 0; j < 3; j++){
@@ -10,6 +10,15 @@ int CreateBoard(){
 	}
 
 }
+void DisplayBoard(board* b){
+for(int i = 0;i<3;i++){
+	for(int j=0; j<3;j++){
+		cout << b[i][j] << " ";
+	}	
+	cout << endl;
+}
+}
+
 int main(){
 	char board[][] = CreateBoard();
 }
